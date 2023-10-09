6fa6d53a918ff332a566c52668950f5404a1b2c0
Heur-a <114075339+Heur-a@users.noreply.github.com>
2023-10-09 19:00:53 +0200

    Added Firebase SDK Auth, CustomLoginActivity added No crash Visca el 9 d'octubre i Visca Carrero Blanco

--- app/src/main/java/com/example/bibliotech/MainActivity.java
+++ app/src/main/java/com/example/bibliotech/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.bibliotech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void cerrarSesion(View view) {
+        AuthUI.getInstance().signOut(getApplicationContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent i = new Intent(
+                                getApplicationContext (),LoginActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(i);
+                        finish();
+                    }
+                });
     }
 }
