e33a1a31072bb4781c2a93aad9cc8459779f5877
smariba <smaribar@epsg.upv.es>
2023-10-22 15:22:22 +0200

    Diseños xml y funciones basicas pantallas: - paginacarga.xml - paginainical.xml - reservas.xml - menu_principal.xml - inicio_sesion.xml - filtro_sala.xml - crear_cuenta.xml - activity_main_final.xml

--- app/src/main/java/com/example/bibliotech/MainActivity.java
+++ app/src/main/java/com/example/bibliotech/MainActivity.java
@@ -4,16 +4,137 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_final);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager2 viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MiPagerAdapter(this));
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    // Configura los íconos y el texto de las pestañas en base a la posición
+                    switch (position) {
+                        case 0:
+                            tab.setIcon(R.drawable.reservassinseleccionar);
+                            break;
+                        case 1:
+                            tab.setIcon(R.drawable.librosinseleccionar);
+                            break;
+                        case 2:
+                            tab.setIcon(R.drawable.casaseleccionada);
+                            break;
+                        case 3:
+                            tab.setIcon(R.drawable.salasinseleccionar);
+                            break;
+                        case 4:
+                            tab.setIcon(R.drawable.mapasinseleccionar);
+                            break;
+                        // Agrega más casos para otras pestañas si es necesario
+                    }
+                }
+        ).attach();
+
+        // Configura el listener para cambiar los íconos cuando se selecciona o deselecciona una pestaña
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Cuando se selecciona una pestaña
+                int position = tab.getPosition();
+                switch (position) {
+                    case 0:
+                        tab.setIcon(R.drawable.reservasseleccionado);
+                        break;
+                    case 1:
+                        tab.setIcon(R.drawable.libroseleccionado);
+                        break;
+                    case 2:
+                        tab.setIcon(R.drawable.casaseleccionada);
+                        break;
+                    case 3:
+                        tab.setIcon(R.drawable.salaseleccionada);
+                        break;
+                    case 4:
+                        tab.setIcon(R.drawable.mapaseleccionado);
+                        break;
+                    // Agrega más casos para otras pestañas si es necesario
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Cuando se deselecciona una pestaña
+                int position = tab.getPosition();
+                switch (position) {
+                    case 0:
+                        tab.setIcon(R.drawable.reservassinseleccionar);
+                        break;
+                    case 1:
+                        tab.setIcon(R.drawable.librosinseleccionar);
+                        break;
+                    case 2:
+                        tab.setIcon(R.drawable.casasinseleccionar);
+                        break;
+                    case 3:
+                        tab.setIcon(R.drawable.salasinseleccionar);
+                        break;
+                    case 4:
+                        tab.setIcon(R.drawable.mapasinseleccionar);
+                        break;
+                    // Agrega más casos para otras pestañas si es necesario
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Acción a realizar si la pestaña seleccionada se vuelve a seleccionar
+            }
+        });
+        viewPager.setCurrentItem(2);
+    }
+
+    public class MiPagerAdapter extends FragmentStateAdapter {
+        public MiPagerAdapter(FragmentActivity activity) {
+            super(activity);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+
+        @Override
+        @NonNull
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new reservasview();
+                case 1:
+                    return new salas();
+                case 2:
+                    return new home();
+                case 3:
+                    return new salas();
+                case 4:
+                    return new salas();
+            }
+            return null;
+        }
     }
 
     public void cerrarSesion(View view) {
