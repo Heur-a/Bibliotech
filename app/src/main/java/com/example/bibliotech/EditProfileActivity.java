7829cae2eeb8e3798a582ea1eb7051657a7ade8b
Heur-a <114075339+Heur-a@users.noreply.github.com>
2023-10-27 01:14:06 +0200

    Editar campos usuario funciona

--- app/src/main/java/com/example/bibliotech/EditProfileActivity.java
+++ app/src/main/java/com/example/bibliotech/EditProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,13 +24,12 @@ public class EditProfileActivity extends AppCompatActivity {
         surnames = findViewById(R.id.textViewSurnames);
         category = findViewById(R.id.textViewCategoryProfile);
         email = findViewById(R.id.textViewEmailProfile);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        Button B = findViewById(R.id.guardar);
         updateNames();
+        B.setOnClickListener(task -> {
+            changeCredentials();
+        });
+
     }
 
     private void updateNames () {
@@ -44,5 +44,58 @@ public class EditProfileActivity extends AppCompatActivity {
         // Open the EditProfileActivity when the "editar" ImageView is clicked
         Intent intent = new Intent(EditProfileActivity.this, perfilActivity.class);
         startActivity(intent);
+    }
+
+
+    private void changeCredentials() {
+        if (verificaCampos()) {
+            String nameString = name.getText().toString();
+            String emailString = email.getText().toString();
+            FireBaseActions.updateEmail(emailString, this);
+            FireBaseActions.updateUsername(nameString);
+
+            // Crea un Bundle per emmagatzemar les dades que vols actualitzar
+            Bundle dadesActualitzades = new Bundle();
+            dadesActualitzades.putString("nouNom", nameString);
+            dadesActualitzades.putString("nouEmail", emailString);
+
+            // Crea un intent per reiniciar l'activitat amb les dades actualitzades
+            Intent intent = new Intent(EditProfileActivity.this, EditProfileActivity.class);
+            intent.putExtras(dadesActualitzades);
+
+            startActivity(intent);
+            finish(); // Tanca l'activitat actual
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Obté les dades actualitzades del Bundle, si existeixen
+        Bundle dadesActualitzades = getIntent().getExtras();
+        if (dadesActualitzades != null) {
+            String nouNom = dadesActualitzades.getString("nouNom");
+            String nouEmail = dadesActualitzades.getString("nouEmail");
+
+            // Actualitza la interfície gràfica amb les noves dades
+            name.setText(nouNom);
+            email.setText(nouEmail);
+        }
+    }
+
+    private boolean verificaCampos() {
+        String mail = email.getText().toString();
+        email.setError(null);
+        boolean isValid = true;
+
+        if (mail.isEmpty()) {
+            email.setError("Enter an email");
+            isValid = false;
+        } else if (!mail.contains("@") || !mail.contains(".")) {
+            email.setError("Invalid email format");
+            isValid = false;
+        }
+        return isValid;
     }
 }
